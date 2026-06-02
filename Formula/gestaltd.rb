9d12877723379210066c598bb0f289f6d5a8351c
Hugh Han <hughhan1@gmail.com>
2026-06-02 13:36:22 -0400

    Update gestaltd formula to v0.0.2-alpha.9

--- Formula/gestaltd.rb
+++ Formula/gestaltd.rb
@@ -3,30 +3,30 @@
 class Gestaltd < Formula
   desc "Gestalt server daemon"
   homepage "https://github.com/valon-technologies/gestalt"
-  version "0.0.2-alpha.8"
+  version "0.0.2-alpha.9"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.8/gestaltd-macos-arm64.tar.gz"
-      sha256 "a969f24142a6266cdde28db266b6e19948f2f25be68adbbacecc28387215255f"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.9/gestaltd-macos-arm64.tar.gz"
+      sha256 "c4a5f7cfa2c3b5d54151a0bd9b96f5ead6905fe62468c9c1dbe31317558279e6"
     end
 
     on_intel do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.8/gestaltd-macos-x86_64.tar.gz"
-      sha256 "c68c4b81a6abb76335d740928819454b744879a96b1d5fd9be45ea9f9a2e4c56"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.9/gestaltd-macos-x86_64.tar.gz"
+      sha256 "ded925a36cede25ae0a92405d044cd02213a70a8a0c929a3ae584482ad5847d0"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.8/gestaltd-linux-arm64.tar.gz"
-      sha256 "061fd3af8135db46c305ca2889977b7a7a11d6db217ce9e4c73ad0b8994032d0"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.9/gestaltd-linux-arm64.tar.gz"
+      sha256 "0d423e65e3d0dfd713e419c8126b616c68826ce8ac098c804d0352eaba1cbe55"
     end
 
     on_intel do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.8/gestaltd-linux-x86_64.tar.gz"
-      sha256 "d665d3257e46b7ab9d47a32f3f48014ef47d2fafb04a6821b10fac7cf7faac58"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.9/gestaltd-linux-x86_64.tar.gz"
+      sha256 "e53e696d5b98e0fc027d835962ca696924b86b926a70c1c0e1d11b9b6bd98a86"
     end
   end
 
